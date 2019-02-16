6ed6d7ec287ca75e26b3e0cc92121a17e32704cc
Sharvil Singh <livrahs@hotmail.com>
2019-02-16 13:31:27 +0530

    First Commmit

---
+++ src/main/java/TechnicalBlog/TechnicalBlogApp.java
@@ -0,0 +1,14 @@
+package TechnicalBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalBlogApp
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TechnicalBlogApp.class, args);
+    }
+
+}
